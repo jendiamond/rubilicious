b29dc0de3295b040f5d94ae0ec76f692ae1af617
Jen Diamond <thejendiamond@gmail.com>
2014-01-19 18:56:18 -0800

    initial commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def phase1
+  end
+
+  def phase2
+  end
+
+  def phase3
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rubilicious::Application.config.secret_key_base = 'c5fbc3af0af45ac8863dd46862910938891c13aec7715ba472877fdbd3ce35989d110deb44f599515978cb70a6c0c8a7945da0c53392c977f76cf90d379e947f'
